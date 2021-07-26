d065f3c3c9d5dfa21c86d8241fcc3389ec53ca4c
刘杨 <liuyang@liuyangdeMacBook-Pro.local>
2021-07-26 09:14:52 +0800

    临时提交

---
+++ LeetCode/topic/topic_5.js
@@ -0,0 +1,35 @@
+/**
+ * 最长回文数
+ * 1. 在LeetCode执行超出时间限制...
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome_1 = function (s) {
+    const string_reverse = str => str.split('').reverse().join('');
+    // 控制匹配字符串长度,最长是字符串长度,最短是1
+    for (let i = s.length; i > 0; i--) {
+        // 控制字符串位置 从0开始
+        for (let j = 0; j <= s.length - i; j++) {
+            let str = s.substr(j, i);
+            if (str === string_reverse(str)) {
+                return str;
+            }
+        }
+    }
+};
+
+/**
+ * 看了评论,才发现自己想简单了,没有利用回文字符串的特性
+ * 回文字符串,中心对称,改进思路找中心点,向两边扩散
+ * @param s
+ * @returns {string}
+ */
+const longestPalindrome = function (s) {
+    for (let i = 0; i < s.length; i++) {
+
+    }
+};
+
+console.log(longestPalindrome('abc'))
+
+module.exports = longestPalindrome;
